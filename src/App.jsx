26a20a66bc94bb47ff8c323febc431e019c50e92
[mciannello] <[mciannello@gmail.com]>
2022-12-10 16:52:12 -0300

    Cambios en el Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./app.scss";
+import "./app.css";
 import Home from "../src/pages/home/Home";
 import Watch from "../src/pages/watch/Watch";
 import Register from "../src/pages/register/Register";
 import Login from "../src/pages/login/Login";
-import Landing from "../src/pages/landing/Landing"
-
+import Landing from "../src/pages/landing/Landing";
+import Header from "../src/components/header/Header";
 
 const App = () => {
   return <Home />;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Header.css';
+import logo from "./logo.png";
+import { useState } from "react";
+
+
+function Header() {
+  const[isScrolled, SetIsScrolled] = useState(false);
+
+// Cuando se scrollea la página, si el PageYOffset es 0 (o sea, está todo arriba, da como resultado "false")
+
+window.onscroll = () =>{
+    SetIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+};
+  return (
+
+    <header className={isScrolled ? "navbar scrolled" : "navbar" }>
+      <div className="encabezado">
+        <h3 className="izq"><img src={logo} alt="Logo" className="logo" /></h3>
+        <nav className="der">
+        <a className="nav-link" href="#" aria-current="page"><button className="btn">Idioma</button></a>
+        <a className="nav-link" href="#"> <button className="btn">Ingresar</button></a>
+        </nav>
+      </div>
+    </header>
+
+
+  )
+}
+export default Header
