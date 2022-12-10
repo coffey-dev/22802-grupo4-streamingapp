1a7eb47307f9b6b53333fd076828558743b73890
[mciannello] <[mciannello@gmail.com]>
2022-12-10 15:34:34 -0300

    agrego el landing base a pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "../src/pages/home/Home";
 import Watch from "../src/pages/watch/Watch";
 import Register from "../src/pages/register/Register";
 import Login from "../src/pages/login/Login";
-
+import Landing from "../src/pages/landing/Landing"
 
 
 const App = () => {
