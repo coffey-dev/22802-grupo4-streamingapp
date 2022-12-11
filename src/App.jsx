b32e14f3ff4254c5480fb90ccb22a96f27bd84a0
[mciannello] <[mciannello@gmail.com]>
2022-12-10 21:56:35 -0300

    Se agrega componentes Header y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,16 @@ import Register from "../src/pages/register/Register";
 import Login from "../src/pages/login/Login";
 import Landing from "../src/pages/landing/Landing";
 import Header from "../src/components/header/Header";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
+  // return(
+  //   <div>
+  //     <Header/>
+  //     <Home />
+  //     <Footer />
+  //   </div>
+  // )
   return <Home />;
 };
 
