183994c1751a145c92ba71b087cd3b590760c05e
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2022-12-16 19:45:17 -0300

    fuenciones de traer peliculas en general y onclick de pelicula en particular

--- src/components/Peliculas.jsx
+++ src/components/Peliculas.jsx
@@ -1,70 +1,161 @@
-import React, { useRef } from 'react';
-import "../assets/stylessheets/Peliculas.scss";
-import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import { useState } from "react";
-import Pelicula from './Pelicula';
+import React, { useEffect, useState } from 'react';
+import '../assets/stylessheets/Peliculas.css';
+import axios from 'axios';
+import YouTube from 'react-youtube';
 
+function Peliculas() {
+  const API_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = '5781dc68edd336a415b02ad15023cdf1';
+  const IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
+  const URL_IMAGE = 'https://image.tmdb.org/t/p/original';
 
+  // variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Cargando Películas..." });
+  const [playing, setPlaying] = useState(false);
 
-export default function Peliculas() {
+  //funcion para realizar la petición por get a la api
+  const traerPeliculas = async(searchKey) =>{
+    const type = searchKey ? "search" : "discover";
+    const {data: {results}, } = await axios.get(`${API_URL}/${type}/movie` , {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      }
+    });
+    setMovies(results);
+    setMovie(results[0]);
 
-  const [isMoved, setIsMoved] = useState(false);
+    if (results.lenght){
+      await traerPelicula(results[0].id)
+    }
 
-  // Se crea un controlador del número de "diapositivas" para que el slider se mueva dentro de los items ingresados //
-  
-  const [slideNumber, setSlideNumber] = useState(0);
-
-// Se llama a useRef para evitar usar documet.getElementByID y se coloca ref en el div de container //
-
-  const listRef = useRef();
-
-
-// cuando se clickea, debería mover 230 px, que es el ancho total de cada item //  
-const handleClick = (direction) =>{
-
-  //Cuando se clickea, cambia el estado de IsMoved a true //
-  setIsMoved(true);
-
-  // Utilizamos un método "getBoundingClientRect" que indica donde se está ubicando el item cuando se mueve -incluidos los pixeles del botón //
-  let distance = listRef.current.getBoundingClientRect().x -50
-if(direction === "left" && slideNumber > 0) {
-  setSlideNumber(slideNumber - 1);
-listRef.current.style.transform = `translateX(${230 + distance}px)`
-}
-
-if(direction === "right" && slideNumber < 5){
-  setSlideNumber(slideNumber + 1);
-  listRef.current.style.transform = `translateX(${-230 + distance}px)`
   }
 
-}
 
+
+  // traer la info de un objeto y mostrar trailer en reproductor
+  const traerPelicula = async(id) =>{
+    const {data} = await axios.get(`${API_URL}/movie/${id}` , {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "video"
+      }
+    })
+
+    if(data.videos && data.videos.results){
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0])
+    }
+    setMovie(data)
+  }
+
+  const seleccionarPelicula = async(movie) => {
+    traerPelicula(movie.id)
+    setMovie(movie)
+    window.scrollTo(0,0)
+  }
+
+  // funcion para buscar peliculas por nombre
+  const buscarPelicula = (e) => {
+    e.preventDefault();
+    traerPeliculas(searchKey);
+  }
+
+  useEffect(() => {
+    traerPeliculas();
+  }, []);
 
   return (
-    <div className='list'>
-        <span className='listTitle'>Continue to watch</span>
-        <div className='wrapper'>
-<ArrowBackIosOutlinedIcon 
-className='sliderArrow left' 
-onClick={()=>handleClick("left")}
-
-// Cuando el valor de IsMoved es falso, el botón pasa a ser "none" y no se muestra //
-style={{display: !isMoved && "none"} }
-/>
-
-<div className='container' ref={listRef}>
-<Pelicula index={1}  />
-<Pelicula index={2}  />
-<Pelicula index={3}  />
-<Pelicula index={4}  />
-
-</div>
-
-<ArrowForwardIosOutlinedIcon className='sliderArrow right' onClick={()=>handleClick("right")} />
+    <div>
+      <h2 className='text-center mt-5 mb-5'>Trailer de películas</h2>
+      {/* buscador de peliculas */}
+      <form  className='container mb-4' onSubmit={buscarPelicula}>
+        <div class="input-group">
+          <span class="input-group-text">Búsqueda</span>
+          <input className='text form-control' type="text" aria-label="First name" placeholder='buscar...' onChange={(e) =>
+          setSearchKey(e.target.value)} />
+          <button className='btn btn-primary ms-3'>Buscar</button>
         </div>
+      </form>
+      
+      {/* contenedor del banner y reproductor de video */}
+      <div>
+        <main>
+          {movie ? (
+            <div className='verTrailer' 
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    classname="reproductor container"
+                    containerClassName={"youtube-container amru"}
+                    opts={{
+                      width: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      }
+                    }}
+                    />
+                    <button onClick={() => setPlaying(false)} className="boton">
+                      Cerrar
+                    </button>
+                </>
+              ) : (
+                <div className='container'>
+                  <div className=''>
+                    {trailer ? (
+                      <button 
+                        className='boton'
+                        onClick={()=> setPlaying(true)}
+                        type="button"  
+                      >
+                        Ver trailer
+                      </button>
+                    ) : (
+                      ""
+                      // "Lo siento, no se encuentra el trailer"
+                    )}
+                    <h1 className='text-white'>{movie.title}</h1>
+                    <p className='text-white'>{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
+
+      {/* contenedor que va a mostrar las peliculas actuales */}
+      <div className='container mt-3'>
+        <div className='row'>
+          {movies.map((movie) => (
+            <div key={movie.id} className='col-md-4 mb-3' onClick={()=> seleccionarPelicula(movie)}>
+              <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100%" />
+              <h4 className='text-center'>{movie.title}</h4>
+            </div>
+          ))}
+
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-
+export default Peliculas;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Peliculas from './components/Peliculas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Peliculas />
+    <App />
   </React.StrictMode>
 );
