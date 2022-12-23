9cb8079a4649a6bf755fde21e5328b98e03f7e6e
[mciannello] <[mciannello@gmail.com]>
2022-12-23 10:26:48 -0300

    Cambio en el footer de integrantes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,6 @@ function Footer() {
         <li className="footer_item"><a href="https://www.linkedin.com/in/mciannello" className="nombre_link">Iannello</a></li>
         <li className="footer_item"><a href="https://www.linkedin.com/in/pablo-mandile/" className="nombre_link">Mandile</a></li>
         <li className="footer_item"><a href="https://www.linkedin.com/in/mavitnicolas/" className="nombre_link">Mavit</a></li>
-        <li className="footer_item"><a href="https://www.linkedin.com/in/antonella-paiz-loker/" className="nombre_link">Paiz Loker</a></li>
-        <li className="footer_item"><a href="https://www.linkedin.com/in/victor-hugo-pasero-a56239121/" className="nombre_link">Vazquez</a></li>
       </ul>
       <p className="text-center text-muted">G4© 2022 </p>
     </footer>
