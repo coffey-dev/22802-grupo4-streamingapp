32d8fe8b3ab72e4b868f8130af48912e4e01c372
PoeDRC <106444677+PoeDRC@users.noreply.github.com>
2022-12-26 22:53:29 -0300

    ValidacionLogin

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/_log.scss";
 import MailSharpIcon from '@mui/icons-material/MailSharp';
 import HttpsSharpIcon from '@mui/icons-material/HttpsSharp';
+import validation from "./register/validation.js"
+
+
 
 function Log() {
+
+  const [values, setValues] = useState({
+    email: "",
+    pass: ""
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); 
+    setErrors(validation(values));
+  };
+  
   return (
     <div className="container w-25">
       <div className="row text-white">
@@ -16,7 +40,11 @@ function Log() {
               placeholder="nombre@ejemplo.com"
               className="form-control"
               type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange} 
             ></input>
+            {errors.email && <p className="error">{errors.email}</p>}
           </div>
           <div className="mb-4">
             <label for="password" className="form-label"><div className="log-icon"><HttpsSharpIcon/> Contraseña</div></label>
@@ -24,10 +52,14 @@ function Log() {
               placeholder="••••••••••••••"
               className="form-control"
               type="password"
+              name="pass"
+              value={values.pass}
+              onChange={handleChange} 
             ></input>
+            {errors.pass && <p className="error">{errors.pass}</p>}
           </div>
           <div className="d-grid mb-4">
-            <button type="submit" className="btn btnIniciarSesion fw-bold">Iniciar sesión</button>
+            <button type="submit" className="btn btnIniciarSesion fw-bold" onClick={handleFormSubmit}>Iniciar sesión</button>
           </div>
           <div className="d-grid">
             <button type="submit" className="btn btnRecuperarContraseña fw-bold">Recuperar contraseña</button>
